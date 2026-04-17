4460dd6467115c5f3275169c2a22be464794aee5
Daniel Engel <daniel.ngl.mail@gmail.com>
2026-04-17 09:13:46 +0200

    Add dummy config

---
+++ include/lishp/lishp_config.h
@@ -0,0 +1,15 @@
+#ifndef LISHP_CONFIG_H
+#define LISHP_CONFIG_H
+
+/*****************************************************************************/
+/*                                   TYPES                                   */
+/*****************************************************************************/
+
+typedef struct LISHP_Config LISHP_Config;
+
+struct LISHP_Config
+{
+  int dummy;
+};
+
+#endif /* LISHP_CONFIG_H */
--- include/lishp/lishp_state.h
+++ include/lishp/lishp_state.h
@@ -1,6 +1,10 @@
 #ifndef LISHP_STATE_H
 #define LISHP_STATE_H
 
+#include "lishp_config.h"
+
+#include <assert.h>
+
 /*****************************************************************************/
 /*                                   TYPES                                   */
 /*****************************************************************************/
@@ -9,7 +13,7 @@ typedef struct LISHP_State LISHP_State;
 
 struct LISHP_State
 {
-  int dummy;
+  LISHP_Config* config;
 };
 
 /*****************************************************************************/
@@ -20,5 +24,16 @@ struct LISHP_State
 LISHP_State_Create();
 void
 LISHP_State_Destroy(LISHP_State* state);
+
+/*****************************************************************************/
+/*                                 ACCESSORS                                 */
+/*****************************************************************************/
+
+[[nodiscard, maybe_unused]] static inline LISHP_Config*
+LISHP_State_GetConfig_mut(LISHP_State* state)
+{
+  assert(state);
+  return state->config;
+}
 
 #endif /* LISHP_STATE_H */
