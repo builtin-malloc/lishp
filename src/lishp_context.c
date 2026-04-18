caa7e932604e526633092e9702b904d7431895e7
Daniel Engel <daniel.ngl.mail@gmail.com>
2026-04-18 08:07:59 +0200

    Add thread-local context

---
+++ src/lishp_context.c
@@ -0,0 +1,61 @@
+#include "lishp/lishp_context.h"
+#include "lishp/lishp_alloc.h"
+#include "lishp/lishp_diag.h"
+#include "lishp/lishp_prelude.h"
+#include "lishp/lishp_read.h"
+#include "lishp/lishp_runtime.h"
+#include "lishp/lishp_vm.h"
+#include "lishp/lishp_write.h"
+
+#include <string.h>
+
+/*****************************************************************************/
+/*                                 LIFE CYCLE                                */
+/*****************************************************************************/
+
+[[nodiscard]] LISHP_Context*
+LISHP_Context_Create(LISHP_Runtime* runtime)
+{
+  if (!runtime) return nullptr;
+
+  auto alloc  = LISHP_Runtime_GetAlloc(runtime);
+  auto config = LISHP_Runtime_GetConfig(runtime);
+  auto gc     = LISHP_Runtime_GetGC(runtime);
+
+  LISHP_Context* ctx = LISHP_Alloc_Malloc(alloc, sizeof(*ctx));
+  if (!ctx) goto failure;
+
+  memset(ctx, 0, sizeof(*ctx));
+
+  ctx->diag = LISHP_Diag_Create(alloc, config);
+  if (!ctx->diag) goto failure;
+
+  ctx->vm = LISHP_VM_Create(alloc, config);
+  if (!ctx->vm) goto failure;
+
+  ctx->reader = LISHP_Reader_Create(alloc, gc);
+  if (!ctx->reader) goto failure;
+
+  ctx->writer = LISHP_Writer_Create(alloc);
+  if (!ctx->writer) goto failure;
+
+  return ctx;
+
+failure:
+  // TODO: Print Error
+  LISHP_Context_Destroy(ctx);
+  return nullptr;
+}
+
+void
+LISHP_Context_Destroy(LISHP_Context* ctx)
+{
+  if (!ctx) return;
+
+  LISHP_Diag_Destroy(ctx->diag);
+  LISHP_VM_Destroy(ctx->vm);
+  LISHP_Reader_Destroy(ctx->reader);
+  LISHP_Writer_Destroy(ctx->writer);
+
+  LISHP_Alloc_Free(LISHP_Context_GetAlloc(ctx), ctx);
+}
--- src/main.c
+++ src/main.c
@@ -1,18 +1,25 @@
 #include <stdlib.h>
 
 #include "lishp/lishp_alloc.h"
+#include "lishp/lishp_context.h"
 #include "lishp/lishp_runtime.h"
-#include "lishp/lishp_state.h"
 
 int
 main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-  auto runtime = LISHP_Runtime_Create(LISHP_GetHeapAlloc());
-  if (!runtime) return EXIT_FAILURE;
-  auto state = LISHP_State_Create(LISHP_GetHeapAlloc());
-  if (!state) return EXIT_FAILURE;
+  auto exit_code = EXIT_SUCCESS;
+  auto alloc     = LISHP_GetHeapAlloc();
+  auto runtime   = LISHP_Runtime_Create(alloc);
+  auto context   = LISHP_Context_Create(runtime);
 
+  if (!runtime || !context) {
+    exit_code = EXIT_FAILURE;
+    goto exit;
+  }
+
+exit:
+  LISHP_Context_Destroy(context);
   LISHP_Runtime_Destroy(runtime);
-  LISHP_State_Destroy(state);
-  return EXIT_SUCCESS;
+
+  return exit_code;
 }
