e28b690903f2706f99d715da8c6d2ebc7128041b
Daniel Engel <daniel.ngl.mail@gmail.com>
2026-04-27 10:35:44 +0200

    Set up basic test macros

---
+++ include/lishp/lishp_test.h
@@ -0,0 +1,25 @@
+#ifndef LISHP_TEST_H
+#define LISHP_TEST_H
+
+/*****************************************************************************/
+/*                                   MACROS                                  */
+/*****************************************************************************/
+
+#define LISHP_TEST(Suite, Name)                                                \
+  [[maybe_unused]] static void LISH_TEST__##Suite##__##Name(                   \
+    [[maybe_unused]] LISHP_TestContext* LISHP_TEST_CONTEXT)
+
+/*****************************************************************************/
+/*                                   TYPES                                   */
+/*****************************************************************************/
+
+typedef struct LISHP_TestContext LISHP_TestContext;
+
+/*****************************************************************************/
+/*                                    MAIN                                   */
+/*****************************************************************************/
+
+[[nodiscard]] int
+LISHP_TestMain(int argc, char* argv[]);
+
+#endif /* LISHP_TEST_H */
--- test/test_main.c
+++ test/test_main.c
@@ -1,6 +1,23 @@
-int
-main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
-{
+#include "lishp/lishp_test.h"
 
-  return 0;
+#include <stdlib.h>
+
+/*****************************************************************************/
+/*                                    MAIN                                   */
+/*****************************************************************************/
+
+int
+main(int argc, char* argv[])
+{
+  return LISHP_TestMain(argc, argv);
+}
+
+/*****************************************************************************/
+/*                            MAIN IMPLEMENTATION                            */
+/*****************************************************************************/
+
+[[nodiscard]] int
+LISHP_TestMain([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+  return EXIT_SUCCESS;
 }
